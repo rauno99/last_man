d3459990962759f485c798d590c4f603a2e5829a
rauno99 <rauno.polluaar@gmail.com>
2022-02-16 17:04:02 +0200

    frontend ui changes

--- last_man_standing/src/App.vue
+++ last_man_standing/src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div id="app">
-    
+    <b-container fluid>
+    <b-icon class="h1 mb-1" v-b-toggle.sidebar-backdrop icon="justify"></b-icon>
+    <b-sidebar
+      id="sidebar-backdrop"
+      title="Viimane jöldak püsti"
+      backdrop-variant="dark"
+      backdrop
+      bg-variant="dark"
+      text-variant="light"
+      shadow
+    >
+      <div class="px-3 py-2">
+      </div>
+    </b-sidebar>
     <router-view/>
+    </b-container>
   </div>
 </template>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+  .text {
+    font-family: 'Roboto', sans-serif;
+    color: gray;
+    font-weight: 300;
+  }
+  html, body {
+    background-color: black;
+  }
+
+  #app {
+    background-color: black;
+  }
 
 </style>
--- last_man_standing/src/components/Voting.vue
+++ last_man_standing/src/components/Voting.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <h2>Seistud aeg</h2>
-    <h1>{{ times.stopwatch }} </h1>
-    <div>
-    <h2>Järgmise ülesandeni</h2>
-    <h1>{{ times.timer }} </h1>
-  </div>
+    <b-container fluid>
+      <b-row class=text-center>
+        <b-col>
+          <h2 class="timeTitle">Seistud aeg</h2>
+          <h1 class="time" v-if="stopper.stopwatch==='NaN:NaN:NaN'">00:00:00</h1>
+          <h1 class="time" v-else>{{ stopper.stopwatch }}</h1>
+        </b-col>
+        <b-col>
+          <h2 class="timeTitle">Järgmise ülesandeni</h2>
+          <h1 class="time" v-if="timer.timer==='NaN:NaN:NaN'">00:00:00</h1>
+          <h1 v-else class="time">{{ timer.timer }}</h1>
+        </b-col>
+      </b-row>
+    <b-button @click="startStopper">start stopper</b-button>
+    <b-button @click="startTimer">start</b-button>
+    <b-button @click="resetTimer">reset</b-button>
+    <b-button @click="resumeTimer">resume</b-button>
+    <b-button @click="stopTimer">stop</b-button>
+    </b-container>
   </div>
 </template>
 
@@ -16,22 +29,60 @@ export default {
   name: 'Voting',
   data() {
     return {
-      times: '',
+      timer: '',
+      stopper: '',
       timePollInterval: null,
     }
   },
   methods: {
     getTimes: function() {
-      axios.get('http://192.168.1.156:5000/getTimes')
-      .then(res => (this.times = res.data));
+      axios.get('http://192.168.1.156:5000/stopper/time')
+      .then(res => (this.stopper = res.data));
+      axios.get('http://192.168.1.156:5000/timer/time')
+      .then(res => (this.timer = res.data));
+    },
+    startStopper: function() {
+      axios.post('http://192.168.1.156:5000/stopper/start')
+    },
+    resetStopper: function() {
+      axios.post('http://192.168.1.156:5000/stopper/reset')
+    },
+    stopStopper: function() {
+      axios.post('http://192.168.1.156:5000/stopper/stop')
+    },
+    startTimer: function() {
+      axios.post('http://192.168.1.156:5000/timer/start')
+    },
+    resumeTimer: function() {
+      axios.post('http://192.168.1.156:5000/timer/resume')
+    },
+    resetTimer: function() {
+      axios.post('http://192.168.1.156:5000/timer/reset')
+    },
+    stopTimer: function() {
+      axios.post('http://192.168.1.156:5000/timer/stop')
     },
   },
 
-  created() {
-    this.timePollInterval = setInterval(() => this.getTimes(), 990);
+  mounted() {
+    this.timePollInterval = setInterval(() => this.getTimes(), 500);
   },
 }
 </script>
 
-<style scoped>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+  .time {
+    font-family: 'Roboto', sans-serif;
+    color: gray;
+    font-weight: 300;
+    font-size: 600%;
+  }
+
+  .timeTitle {
+    font-family: 'Roboto', sans-serif;
+    color: gray;
+    font-weight: 300;
+    font-size: 200%;
+  }
 </style>
