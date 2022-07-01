fc27823de2af11abe8e74f545f9108e4a3f565f7
rauno99 <rauno.polluaar@gmail.com>
2022-07-01 15:27:41 +0300

    db fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 #MVP-d: Markus, Markus, Samsungi monitor, huge ass chonky Samsungi monitor, Robert
 
-import time, json, random
+import time, json, random, os
 from math import floor
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
@@ -18,7 +18,10 @@ CORS(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/last_man_db'
 
 # Heroku database URL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://eupjeijraobnwo:19ee1d4919796a724bd693ee2803d20edbbf02dca5abe14fb27402b0949362ad@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dbpsjih91uudnl'
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://eupjeijraobnwo:19ee1d4919796a724bd693ee2803d20edbbf02dca5abe14fb27402b0949362ad@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dbpsjih91uudnl'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
